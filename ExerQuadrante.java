ff3c2d2c4ef8ac5827b9dea882ab5a516165ea2d
Junior Zandrowski <juniorzandrowski@hotmail.com>
2020-06-10 15:05:40 -0300

    Commit

---
+++ ExerQuadrante.java
@@ -0,0 +1,37 @@
+
+public class ExerQuadrante {
+
+	public static void main(String[] args) {
+		
+		int x= -3;
+		int y= 4;
+		
+		if ((x > 0) && (y > 0))
+		{
+			System.out.println("primeiro quadrante"); 
+		}
+
+		if ((x < 0) && (y < 0))
+		{
+			System.out.println("terceiro quadrante"); 
+		}
+
+		if ((x < 0) && (y > 0))
+		{
+			System.out.println("segundo quadrante"); 
+		}
+
+		if ((x > 0) && (y < 0))
+		{
+			System.out.println("quarto quadrante");
+		}
+		
+	}
+
+		
+		
+		
+		
+		
+	}
+	
---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,17 @@
+public class OperadoresAritmeticos {
+	
+	public static void main(String[] args) {
+		
+		int a=4;
+		int b=6;
+		int c=3;
+		
+		System.out.println(a+b);
+		System.out.println(a+c*b);
+		System.out.println(c*c);
+		System.out.println(b/c);
+		System.out.println(b%c);
+		System.out.println((a+b)*(a-b));
+		
+	}
+}
